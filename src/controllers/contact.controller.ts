ca43f6c5aa273df8ecfe2151826e19032966a16a
syazah <thesyazah@gmail.com>
2024-11-15 10:02:15 +0530

    Added Get Contact and Update Contact API endpoints

--- src/controllers/contact.controller.ts
+++ src/controllers/contact.controller.ts
@@ -1,15 +1,19 @@
 import { PrismaClient } from "@prisma/client";
+import { Request } from "express";
 import ErrorHandler from "../utils/errorHandler";
 import {
   ContactCreationSchema,
   ContactCreationType,
 } from "../validation/contact.validation";
+import { NextFunction } from "express";
 
 const prisma = new PrismaClient();
+
+// ADDING A NEW CONTACT
 export const AddNewContactController = async (
-  req: any,
+  req: Request,
   res: any,
-  next: any
+  next: NextFunction
 ) => {
   try {
     const RequestBody: ContactCreationType = req.body;
@@ -46,6 +50,62 @@ export const AddNewContactController = async (
       );
     }
     return res.status(201).json({ success: true, data: user });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+//GETTING CONTACTS
+export const GetAllContactsController = async (
+  req: Request,
+  res: any,
+  next: NextFunction
+) => {
+  try {
+    const contacts = await prisma.contact.findMany({});
+    if (!contacts || contacts.length === 0) {
+      return next(
+        ErrorHandler(
+          500,
+          "Something went wrong while fetching data from the database"
+        )
+      );
+    }
+    return res.status(200).json({ success: true, data: contacts });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+//UPDATING CONTACT'S INFO
+export const UpdateContactInfoController = async (
+  req: Request,
+  res: any,
+  next: NextFunction
+) => {
+  try {
+    const RequestBody: ContactCreationType = req.body;
+    const validate = ContactCreationSchema.safeParse(RequestBody);
+    if (!validate.success) {
+      return next(ErrorHandler(400, validate.error.errors[0].message));
+    }
+    const RequestParam = req.params;
+    if (!RequestParam.id) {
+      return next(ErrorHandler(400, "No id for contact is found"));
+    }
+
+    const updatedContact = await prisma.contact.update({
+      where: { id: Number(RequestParam.id) },
+      data: {
+        firstName: RequestBody.firstName,
+        lastName: RequestBody?.lastName,
+        email: RequestBody.email,
+        phoneNumber: RequestBody.phoneNumber,
+        company: RequestBody.company,
+        jobTitle: RequestBody.jobTitle,
+      },
+    });
+    return res.status(200).json({ success: true, data: updatedContact });
   } catch (error) {
     return next(error);
   }
--- src/validation/contact.validation.ts
+++ src/validation/contact.validation.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 const ContactCreationSchema = z.object({
   firstName: z.string(),
-  lastName: z.string(),
+  lastName: z.string().optional(),
   email: z.string().email(),
   phoneNumber: z.string(),
   company: z.string(),
